a57a7c542af9990019749b65910cd2550777967e
daniel <daniel@gmail.com>
2026-09-11 06:44:44 -0500

    Paso 6

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const formulario = document.querySelector("#formEstudiante");
+
+formulario.addEventListener("submit", function(event) {
+    event.preventDefault();
+    console.log("Formulario enviado");
+});
+
